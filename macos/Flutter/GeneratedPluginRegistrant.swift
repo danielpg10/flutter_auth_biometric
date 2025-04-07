a53bbc9c926a93b96cf69ec9049c51c876726585
Daniel <danielpg2020md@gmail.com>
2025-04-07 10:27:18 -0500

    refactor: Implement clean architecture with bloc pattern

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import local_auth_darwin
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
